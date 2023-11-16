8eda5c03951704fbc7d7ab190895da478151cfc5
Michael Loughman <mcloughman@gmail.com>
2023-11-15 20:03:06 -0800

    Create a selectedAccount state. We default it to the first account in the accounts array. This eliminates the need for nested mapping when rendering the spendings-div children. So it simplified the code greatly from the last commit. The unintended consequences were that we lost the dynamic width styling that we had previously. We finally create a divWidth state. And we create a widths variable which we use reduce method on  our selectedAccount.spendings to set widths. We set value of divWidths tp be widths. Then we use the indexes in our style object.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
-
 import accounts from "../data"
+
 const Home = () => {
-  console.log(accounts)
+  const [selectedAccount, setSelectedAccount] = useState(accounts[0])
+  // so after a mulitple hour session, we finally found the way to render the widths of the divs dynamically. AI tried so many ways to make it happen and finally we were able to do it. I knew that using state might be helpful. And in the end it was. But so painful. We tried to create good widths array using .map. But it didn't work. Then AI suggested using the reducer. And FINALLY it worked. This was all brought on by a refactoring of code since last commit. BUT the cost was a few hours of trying to get the dynamic widths back.
+  const [divWidths, setDivWidths] = useState(widths)
+
+  const widths = selectedAccount.spendings.reduce((acc, item, index) => {
+    const width = index === 0 ? 100 : acc[index - 1] * 0.85
+    return [...acc, width]
+  }, [])
+
+  console.log(divWidths)
+  console.log(widths)
+
+  const handleAccountClick = (account) => {
+    setSelectedAccount(account)
+  }
   return (
     <>
       <div className="pay-transfer-div">
@@ -18,8 +32,14 @@ const Home = () => {
           {accounts.map((account) => {
             const { title, balance, id } = account
             const accountTitle = title.split(" ")[0].toLowerCase()
+            // Dynamically set the class name based on whether it's the selected account
+            const isSelected = account === selectedAccount
             return (
-              <div className={`${accountTitle}-div`} key={id}>
+              <div
+                className={`account-div ${isSelected ? "selected" : ""}`}
+                key={id}
+                onClick={() => handleAccountClick(account)}
+              >
                 <span>{title}</span>
                 <span>$ {balance}</span>
               </div>
@@ -28,30 +48,20 @@ const Home = () => {
         </div>
         <div className="spendings-div">
           <h2>Spendings</h2>
-          {accounts.map((account) => {
-            const { title, id } = account
-            // this will help with naming the title divs
-            const accountTitle = title.split(" ")[0].toLowerCase()
-            let width = 100 // we're going to use this so set the div widths as percentages
+
+          {selectedAccount.spendings.map((item, index) => {
+            console.log(index)
+            const { category, spent } = item
+            const divCategory = category.toLowerCase()
+
             return (
-              <div key={id} className={`${accountTitle}-account-div`}>
-                {account.spendings.map((item) => {
-                  const { category, spent } = item
-                  const divCategory = category.toLowerCase()
-                  const style = { width: `${width}%` }
-                  // Adjust width for the next iteration
-                  width *= 0.85
-                  return (
-                    <div
-                      className={`${divCategory}-div`}
-                      key={category}
-                      style={style}
-                    >
-                      <span>{category}</span>
-                      <span>${spent}</span>
-                    </div>
-                  )
-                })}
+              <div
+                className={`${divCategory}-div`}
+                key={category}
+                style={{ width: `${divWidths[index]}%` }}
+              >
+                <span>{category}</span>
+                <span>${spent}</span>
               </div>
             )
           })}
