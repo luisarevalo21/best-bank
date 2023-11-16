4844a9897c08f7cda2da55d132b8a004e1506205
Michael Loughman <mcloughman@gmail.com>
2023-11-15 16:03:51 -0800

    Create home page for initial rendering of the home page. Map through the data once each for the accounts side and the spending side. There's also some nested mapping on the spending side. This made for much dynamic styling and class naming. But it looks like the Figma app and is displaying the data. At this point, we are just hiding the expenses account data and defaulting to the main account data as in the desing. Next, we will create some state and event listeners to handle clicks and give some functionality.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from "react"
 
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom"
 
-import logo from "../assets/bestbank_logo.png";
+import logo from "../assets/bestbank_logo.png"
 const Navbar = () => {
   return (
     <>
@@ -11,26 +11,41 @@ const Navbar = () => {
 
         <nav>
           <ul>
-            <NavLink className={({ isActive }) => (isActive ? "activeClass" : "")} to="/">
+            <NavLink
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+              to="/"
+            >
               Home
             </NavLink>
-            <NavLink className={({ isActive }) => (isActive ? "activeClass" : "")} to="/payments">
+            <NavLink
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+              to="/payments"
+            >
               Payments
             </NavLink>
-            <NavLink className={({ isActive }) => (isActive ? "activeClass" : "")} to="/savings">
+            <NavLink
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+              to="/savings"
+            >
               Savings
             </NavLink>
-            <NavLink className={({ isActive }) => (isActive ? "activeClass" : "")} to="/financing">
+            <NavLink
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+              to="/financing"
+            >
               Financing
             </NavLink>
-            <NavLink className={({ isActive }) => (isActive ? "activeClass" : "")} to="/stocks">
+            <NavLink
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+              to="/stocks"
+            >
               Stocks
             </NavLink>
           </ul>
         </nav>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import accounts from "../data";
+import accounts from "../data"
 const Home = () => {
-  console.log(accounts);
-  return <div> home page</div>;
-};
+  console.log(accounts)
+  return (
+    <>
+      <div className="pay-transfer-div">
+        <Link to="payments" className="pay-link">
+          Pay
+        </Link>
+        <Link to="#">Transfer</Link>
+      </div>
+      <div className="accounts-spending-div">
+        <div className="accounts-div">
+          <h2>Accounts</h2>
+          {accounts.map((account) => {
+            const { title, balance, id } = account
+            const accountTitle = title.split(" ")[0].toLowerCase()
+            return (
+              <div className={`${accountTitle}-div`} key={id}>
+                <span>{title}</span>
+                <span>$ {balance}</span>
+              </div>
+            )
+          })}
+        </div>
+        <div className="spendings-div">
+          <h2>Spendings</h2>
+          {accounts.map((account) => {
+            const { title, id } = account
+            // this will help with naming the title divs
+            const accountTitle = title.split(" ")[0].toLowerCase()
+            let width = 100 // we're going to use this so set the div widths as percentages
+            return (
+              <div key={id} className={`${accountTitle}-account-div`}>
+                {account.spendings.map((item) => {
+                  const { category, spent } = item
+                  const divCategory = category.toLowerCase()
+                  const style = { width: `${width}%` }
+                  // Adjust width for the next iteration
+                  width *= 0.85
+                  return (
+                    <div
+                      className={`${divCategory}-div`}
+                      key={category}
+                      style={style}
+                    >
+                      <span>{category}</span>
+                      <span>${spent}</span>
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default Home;
+export default Home
